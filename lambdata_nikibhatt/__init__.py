2a24bfd15959135d209b5522c0f0cd7638567b42
nikibhatt <nkibhatt@users.noreply.github.com>
2019-12-03 11:01:22 -0800

    added lambdata_nikibhatt

---
+++ lambdata_nikibhatt/__init__.py
@@ -0,0 +1,11 @@
+""""Collection of Data Science helper functions"""
+
+import pandas as pd
+import numpy as np
+
+#Example datasets
+ONES = pd.DataFrame(np.ones(10))
+
+#example funnction
+def increment(x):
+    return(x+1)
