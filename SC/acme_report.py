eee2e66978034a53bf061b20424ce107be9f1359
nikibhatt <nkibhatt@users.noreply.github.com>
2019-12-06 11:37:14 -0800

    Updated report

--- SC/acme_report.py
+++ SC/acme_report.py
@@ -23,9 +23,22 @@ def generate_products(num_prods=30):
 
 def inventory_report(list):
     print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
-    print('Number of products', len(list))
-    #print('Average price of the products', sum(list)/len(list))
-
+    print('Number of products: ', len(list))
+    sum_price = 0
+    for obj in list:
+      sum_price += obj.price
+      ave_price = sum_price/len(list)
+    print('Average price: ', ave_price)
+    sum_weight = 0
+    for obj in list:
+      sum_weight += obj.weight
+      ave_weight = sum_weight/len(list)
+    print('Average weight: ', ave_weight)
+    sum_flammability = 0
+    for obj in list:
+      sum_flammability += obj.flammability
+      ave_flammability = sum_flammability/len(list)
+    print('Average flammability: ', ave_flammability)
 
 if __name__ == '__main__':
     inventory_report(generate_products())
