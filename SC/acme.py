589a15f8a9f85d3c185af84e6400fe2d6747fc86
nikibhatt <nkibhatt@users.noreply.github.com>
2019-12-06 09:22:56 -0800

    Added methods

--- SC/acme.py
+++ SC/acme.py
@@ -9,3 +9,23 @@ class Product:
         self.weight = weight
         self.flammability = flammability
         self.identifier = rand.randint(1000000,10000000)
+
+    def stealability(self):
+        """ Check the stealability based on the price divided by the weight"""
+        ratio = self.price/self.weight
+        if ratio < 0.5:
+          message = 'Not so stealable...'
+        elif ratio >= 0.5 and ratio < 1.0:
+          message = 'Kinda stealable'
+        else: message = 'Very stealable!'
+        print(message)
+
+    def explode(self):
+        """ Check the explosiveness based on the flammability times the weight"""
+        prod = self.flammability * self.weight
+        if prod < 10:
+          message = '...fizzle'
+        elif prod >= 10 and prod < 50:
+          message = '...boom!'
+        else: message = '...BABOOM!!'
+        print(message)
